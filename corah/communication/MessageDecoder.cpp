b1af93e4f9fd8b27c8d54635091d716637c3a764
androst <a.ostvik@gmail.com>
2018-07-06 16:12:43 +0200

    Moved some tcp segment utility functions to parent class.

--- corah/communication/MessageDecoder.cpp
+++ corah/communication/MessageDecoder.cpp
@@ -3,3 +3,45 @@
 //
 
 #include "MessageDecoder.h"
+#include <QDataStream>
+
+
+double MessageDecoder::pickDouble(QByteArray data, int index)
+{
+    double a;
+    QDataStream stream(data.mid(index,sizeof(double)));
+    stream >> a;
+    return a;
+}
+
+QByteArray MessageDecoder::getHeader(QByteArray data,int pos)
+{
+    return data.mid(pos,sizeof(int)+sizeof(char));
+}
+
+QByteArray MessageDecoder::slicePacket(QByteArray data,int pos, int length)
+{
+    return data.mid(pos,length);
+}
+
+int MessageDecoder::headerLength(QByteArray data)
+{
+    return pickInteger(data,0);
+}
+
+int MessageDecoder::pickInteger(QByteArray data,int pos)
+{
+    bool ok;
+    return data.mid(pos,sizeof(int)).toHex().toInt(&ok,16);
+}
+
+int MessageDecoder::headerID(QByteArray data)
+{
+    bool ok;
+    return data.mid((sizeof(int)),sizeof(char)).toHex().toInt(&ok,16);
+}
+
+QByteArray MessageDecoder::removeHeader(QByteArray data)
+{
+    return data.mid(sizeof(int)+sizeof(char),data.size()-sizeof(int)-sizeof(char));
+}
--- corah/communication/MessageDecoder.h
+++ corah/communication/MessageDecoder.h
@@ -2,17 +2,33 @@
 // Created by androst on 02.07.18.
 //
 
-#include <QObject>
+#include <utilities/corahHeaders.h>
 
 #include "robotics/RobotState.h"
 
 #ifndef CORAH_MESSAGEDECODER_H
 #define CORAH_MESSAGEDECODER_H
 
+struct JointState
+{
+    Eigen::RowVectorXd jointConfiguration;
+    Eigen::RowVectorXd jointVelocity;
+    double timestamp;
+};
 
 class MessageDecoder {
 public:
-    virtual RobotState analyzeRawPacket(QByteArray packet) = 0;
+    virtual JointState analyzeTCPSegment(QByteArray packet) = 0;
+
+    QByteArray slicePacket(QByteArray data, int pos, int length);
+    QByteArray getHeader(QByteArray data,int pos);
+    QByteArray removeHeader(QByteArray data);
+
+    double pickDouble(QByteArray data, int pos);
+    int pickInteger(QByteArray data, int pos);
+    int headerLength(QByteArray data);
+    int headerID(QByteArray data);
+
 };
 
 
