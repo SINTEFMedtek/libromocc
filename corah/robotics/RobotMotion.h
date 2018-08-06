56edada4c8ad80dc93a8046f2d206e4a90dd4339
androst <a.ostvik@gmail.com>
2018-08-06 16:20:40 +0200

    Started work on structure for handling robot motion.

---
+++ corah/robotics/RobotMotion.h
@@ -0,0 +1,21 @@
+#ifndef ROBOTMOTION_H
+#define ROBOTMOTION_H
+
+#include <utilities/corahHeaders.h>
+
+/**
+ * Struct that holds robot motion information.
+ *
+ * \ingroup org_custusx_robotics
+ *
+ * \author Andreas Østvik
+ * \date 2015-07-10
+ */
+
+
+class RobotMotion {
+
+};
+
+
+#endif //ROBOTMOTION_H
