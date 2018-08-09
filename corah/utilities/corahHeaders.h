2aa9f725c199f8e5c7fb59f9c5cb4b886adbe656
androst <a.ostvik@gmail.com>
2018-08-09 13:54:11 +0200

    Added a typedef for 6 dim column vector.

--- corah/utilities/corahHeaders.h
+++ corah/utilities/corahHeaders.h
@@ -16,6 +16,7 @@
 
 // Orocos KDL
 #include <chain.hpp>
+#include <chainjnttojacsolver.hpp>
 #include <chainfksolverpos_recursive.hpp>
 #include <chainiksolverpos_nr.hpp>
 #include <chainiksolvervel_pinv.hpp>
@@ -23,8 +24,19 @@
 // Corah utilities
 #include <utilities/corahHelpers.h>
 
+
 // Supported manipulators
 typedef enum {UR5} Manipulator;
+
+// Robot motion
 typedef enum {movej,movep,speedj,speedl,stopj,stopl} MotionType;
+typedef std::vector<struct RobotMotion> MotionQueue;
+
+// Eigen robot typedefs
+typedef Eigen::Affine3d Transform3d;
+typedef Eigen::Vector3d Vector3d;
+typedef Eigen::Matrix<double,6,1> Vector6d;
+typedef Eigen::Matrix<double,1,6> RowVector6d;
+typedef Eigen::Matrix<double,6,6> Matrix6d;
 
 #endif //CORAH_CORAHHEADERS_H
