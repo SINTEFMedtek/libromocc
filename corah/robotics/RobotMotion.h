9bac7b6d58f7d3736a771a6ec97edb4fc27e1160
androst <a.ostvik@gmail.com>
2018-08-09 13:58:12 +0200

    Added a stucture for robot motion and a queue class.

--- corah/robotics/RobotMotion.h
+++ corah/robotics/RobotMotion.h
@@ -12,8 +12,41 @@
  * \date 2015-07-10
  */
 
+struct RobotMotion
+{
+    Transform3d targetPose;
+    RowVector6d targetJointConfiguration;
 
-class RobotMotion {
+    double acceleration;
+    double velocity;
+    double time = 0;
+    double blendRadius = 0;
+
+    MotionType motionType;
+};
+
+class RobotMotionQueue
+{
+    public:
+        void add(Transform3d target, MotionType type, double acc, double vel, double time = 0, double blendRadius=0)
+        {
+            RobotMotion motion;
+            motion.targetPose = target;
+            motion.motionType = type;
+            motion.acceleration = acc;
+            motion.velocity = vel;
+            motion.time = time;
+            motion.blendRadius = blendRadius;
+
+            mQueue.push_back(motion);
+        };
+
+        void clear(){mQueue.clear();};
+
+        MotionQueue getQueue(){ return mQueue;};
+
+    private:
+        MotionQueue mQueue;
 
 };
 
