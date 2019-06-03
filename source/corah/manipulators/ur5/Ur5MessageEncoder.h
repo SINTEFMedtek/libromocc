e0eb6400df17e0282b7064c22bd400c0f4af07a1
Andreas Østvik <a.ostvik@gmail.com>
2019-06-03 08:24:57 +0200

    Refactored to use std::string instead of QString.

--- source/corah/manipulators/ur5/Ur5MessageEncoder.h
+++ source/corah/manipulators/ur5/Ur5MessageEncoder.h
@@ -1,8 +1,6 @@
 #ifndef UR5MESSAGEENCODER_H
 #define UR5MESSAGEENCODER_H
 
-#include <QObject>
-
 #include "corah/communication/MessageEncoder.h"
 #include "corah/robotics/RobotState.h"
 
@@ -20,26 +18,26 @@ class CORAH_EXPORT Ur5MessageEncoder : public MessageEncoder
 {
 
 public:
-    virtual QString moveCommand(MotionType type, Eigen::RowVectorXd targetConfiguration, double acc, double vel, double t, double rad);
-    virtual QString stopCommand(MotionType typeOfStop, double acc);
-    virtual QString shutdownCommand();
+    virtual std::string moveCommand(MotionType type, Eigen::RowVectorXd targetConfiguration, double acc, double vel, double t, double rad);
+    virtual std::string stopCommand(MotionType typeOfStop, double acc);
+    virtual std::string shutdownCommand();
 
-    QString moveCommand(MotionType type, Eigen::Affine3d targetPose, double acc, double vel, double t, double rad);
+    std::string moveCommand(MotionType type, Eigen::Affine3d targetPose, double acc, double vel, double t, double rad);
 
 private:
-    QString movej(Eigen::RowVectorXd jointConfig, double a, double v, double t, double r);
-    QString movep(Eigen::RowVectorXd operationalConfig, double a, double v, double t, double r);
+    std::string movej(Eigen::RowVectorXd jointConfig, double a, double v, double t, double r);
+    std::string movep(Eigen::RowVectorXd operationalConfig, double a, double v, double t, double r);
 
-    QString speedj(Eigen::RowVectorXd jointVelocity, double a, double t);
-    QString speedl(Eigen::RowVectorXd operationalVelocity, double a, double t);
+    std::string speedj(Eigen::RowVectorXd jointVelocity, double a, double t);
+    std::string speedl(Eigen::RowVectorXd operationalVelocity, double a, double t);
 
-    QString stopj(double a);
-    QString stopl(double a);
+    std::string stopj(double a);
+    std::string stopl(double a);
 
-    QString textmsg(QString msg);
-    QString sleep(double time);
+    std::string textmsg(std::string msg);
+    std::string sleep(double time);
 
-    QString powerdown();
+    std::string powerdown();
 
 };
 
