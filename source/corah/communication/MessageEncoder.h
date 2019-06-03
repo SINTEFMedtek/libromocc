9d63f1f7a35336e86e4ec163dd0cae7fc2892d59
Andreas Østvik <a.ostvik@gmail.com>
2019-06-03 08:18:57 +0200

    Added a string format utility function.

--- source/corah/communication/MessageEncoder.h
+++ source/corah/communication/MessageEncoder.h
@@ -1,7 +1,6 @@
 #ifndef CORAH_MESSAGEENCODER_H
 #define CORAH_MESSAGEENCODER_H
 
-#include <QObject>
 #include <Eigen/Dense>
 
 #include "corah/utilities/corahHeaders.h"
@@ -12,10 +11,13 @@ namespace corah
 class CORAH_EXPORT MessageEncoder {
 
 public:
-    virtual QString moveCommand(MotionType type, Eigen::RowVectorXd jointConfig, double acc, double vel, double t, double rad) = 0;
-    virtual QString moveCommand(MotionType type, Eigen::Affine3d pose, double acc, double vel, double t, double rad) = 0;
-    virtual QString stopCommand(MotionType type, double acc) = 0;
-    virtual QString shutdownCommand() = 0;
+    virtual std::string moveCommand(MotionType type, Eigen::RowVectorXd jointConfig, double acc, double vel, double t, double rad) = 0;
+    virtual std::string moveCommand(MotionType type, Eigen::Affine3d pose, double acc, double vel, double t, double rad) = 0;
+    virtual std::string stopCommand(MotionType type, double acc) = 0;
+    virtual std::string shutdownCommand() = 0;
+
+protected:
+    std::string format(const std::string& format, ...);
 };
 
 }
