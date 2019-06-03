965fb824992082d0bff2f2ce13c790fb940f8f71
Andreas Østvik <a.ostvik@gmail.com>
2019-06-03 08:09:09 +0200

    Started work on a virtual parent class.

---
+++ source/corah/core/Object.cpp
@@ -0,0 +1,5 @@
+//
+// Created by androst on 02.06.19.
+//
+
+#include "Object.h"
---
+++ source/corah/core/Object.h
@@ -0,0 +1,17 @@
+//
+// Created by androst on 02.06.19.
+//
+
+#ifndef CORAH_OBJECT_H
+#define CORAH_OBJECT_H
+
+
+class Object {
+
+public:
+    virtual void update() = 0;
+
+};
+
+
+#endif //CORAH_OBJECT_H
