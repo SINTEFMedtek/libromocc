09fbd227012bc3b0f73bb5f9f9bd251412341289
Andreas Østvik <a.ostvik@gmail.com>
2019-06-03 08:24:24 +0200

    Refactored to zmq and packet processing. Using struct attributes. Removed Qt functionality.

--- source/corah/manipulators/ur5/Ur5MessageDecoder.cpp
+++ source/corah/manipulators/ur5/Ur5MessageDecoder.cpp
@@ -1,51 +1,26 @@
 #include "Ur5MessageDecoder.h"
-#include <QDebug>
 #include <set>
-#include <QDataStream>
 #include <iostream>
 
 namespace corah
 {
 
-JointState Ur5MessageDecoder::analyzeTCPSegment(QByteArray packet)
+JointState Ur5MessageDecoder::analyzeTCPSegment(unsigned char* packet)
 {
     JointState state;
-    if(headerLength(packet)==812 || headerLength(packet) == 1044)
+    if(packageSize(packet)==812 || packageSize(packet) == 1044)
     {
-        state = setRTState(slicePacket(packet,sizeof(int),headerLength(packet)));
+        raw_ur5_state* raw_state = reinterpret_cast<raw_ur5_state*>(packet);
+
+        double timestamp = toLittleEndian(&raw_state->time_);
+        double* jointConfiguration = arrayToLittleEndian(raw_state->actual_positions_);
+        double* jointVelocity = arrayToLittleEndian(raw_state->actual_velocities_);
+
+        state.timestamp = timestamp;
+        state.jointConfiguration = RowVector6d(jointConfiguration);
+        state.jointVelocity = RowVector6d(jointVelocity);
     }
     return state;
-}
-
-JointState Ur5MessageDecoder::setRTState(QByteArray data)
-{
-    JointState state;
-
-    state.timestamp = pickDouble(data,0);
-    state.jointConfiguration = getJointPositionsRT(data);
-    state.jointVelocity = getJointVelocitiesRT(data);
-
-    return state;
-}
-
-Eigen::RowVectorXd Ur5MessageDecoder::getJointPositionsRT(QByteArray data)
-{
-    Eigen::RowVectorXd jp(6);
-    for(int i=0;i<6;i++)
-    {
-        jp(i) = pickDouble(data,(31+i)*sizeof(double));
-    }
-    return jp;
-}
-
-Eigen::RowVectorXd Ur5MessageDecoder::getJointVelocitiesRT(QByteArray data)
-{
-    Eigen::RowVectorXd jv(6);
-    for(int i=0;i<6;i++)
-    {
-        jv(i) = pickDouble(data,(37+i)*sizeof(double));
-    }
-    return jv;
 }
 
 }
--- source/corah/manipulators/ur5/Ur5MessageDecoder.h
+++ source/corah/manipulators/ur5/Ur5MessageDecoder.h
@@ -1,8 +1,6 @@
 #ifndef UR5RECEIVE_H
 #define UR5RECEIVE_H
 
-#include <QObject>
-
 #include "corah/robotics/RobotState.h"
 #include "corah/communication/MessageDecoder.h"
 
@@ -12,23 +10,58 @@ namespace corah
 /**
  * Class that handles UR5 robot recieved messages.
  *
- * \ingroup org_custusx_robot_ur5
- *
  * \author Andreas Østvik
  *
  */
 
+
 class CORAH_EXPORT Ur5MessageDecoder : public MessageDecoder
 {
 public:
-    virtual JointState analyzeTCPSegment(QByteArray packet);
+    virtual JointState analyzeTCPSegment(unsigned char* packet);
 
 private:
-    JointState setRTState(QByteArray data);
+    static const int NUMBER_OF_JOINTS = 6;
 
-    Eigen::RowVectorXd getJointPositionsRT(QByteArray data);
-    Eigen::RowVectorXd getJointVelocitiesRT(QByteArray data);
+    struct __attribute__((packed)) raw_ur5_state
+    {
+        int32_t message_size_;
+        double time_;
+        double target_positions_[NUMBER_OF_JOINTS];
+        double target_velocities_[NUMBER_OF_JOINTS];
+        double target_accelerations_[NUMBER_OF_JOINTS];
+        double target_currents_[NUMBER_OF_JOINTS];
+        double target_torques_[NUMBER_OF_JOINTS];
+        double actual_positions_[NUMBER_OF_JOINTS];
+        double actual_velocities_[NUMBER_OF_JOINTS];
+        double actual_currents_[NUMBER_OF_JOINTS];
+        double joint_control_torques_[NUMBER_OF_JOINTS];
+        double actual_tool_coordinates_[NUMBER_OF_JOINTS];
+        double actual_tool_speed_[NUMBER_OF_JOINTS];
+        double generalised_tool_forces_[NUMBER_OF_JOINTS];
+        double target_tool_coordinates_[NUMBER_OF_JOINTS];
+        double target_tool_speed_[NUMBER_OF_JOINTS];
+        int64_t digital_pins_;
+        double motor_temperatures_[NUMBER_OF_JOINTS];
+        double controller_timer_;
+        double test_value_;
+        int64_t robot_mode_;
+        int64_t joint_modes_[NUMBER_OF_JOINTS];
+        int64_t safety_mode_;
+        int64_t unused_1_[6];
+        double tool_accelerometer_values_[3];
+        int64_t unused_2_[6];
+        double speed_scaling_;
+        double linear_momentum_norm_;
+        int64_t unused_3_[2];
+        double masterboard_main_voltage_;
+        double masterboard_robot_voltage_;
+        double masterboard_robot_current_;
+        double actual_joint_voltages_[NUMBER_OF_JOINTS];
+    };
 };
+
+
 
 }
 
