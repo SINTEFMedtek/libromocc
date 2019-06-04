8c89793f19a49ece52f8f0255a7818efdfe0d89c
Andreas Østvik <a.ostvik@gmail.com>
2019-06-04 15:19:09 +0200

    Some modifications to core object. Need to remove virtual observer utility.

--- source/romocc/core/Object.h
+++ source/romocc/core/Object.h
@@ -5,13 +5,21 @@
 #ifndef ROMOCC_OBJECT_H
 #define ROMOCC_OBJECT_H
 
+#include "ForwardDeclarations.h"
+#include "SmartPointers.h"
+
+namespace romocc{
 
 class Object {
+    public:
+        typedef SharedPointer<Object> pointer;
+        virtual void update() = 0;
 
-public:
-    virtual void update() = 0;
+    protected:
+        std::weak_ptr<Object> mPtr;
 
 };
 
+} // end namespace romocc
 
 #endif //ROMOCC_OBJECT_H
