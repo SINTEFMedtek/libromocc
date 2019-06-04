2d898d675d5b067b1c1919ba4543ff977c5395ab
Andreas Østvik <a.ostvik@gmail.com>
2019-06-04 15:18:06 +0200

    Added file to include precompiled headers.

---
+++ source/romocc/core/PrecompiledHeaders.h
@@ -0,0 +1,24 @@
+//
+// Created by androst on 06.07.18.
+//
+
+#ifndef ROMOCC_PRECOMPILEDHEADERS_H
+#define ROMOCC_PRECOMPILEDHEADERS_H
+
+// Standard library
+#include <memory>
+
+// Eigen
+#include <Eigen/Dense>
+
+// Orocos KDL
+#include <kdl/chain.hpp>
+#include <kdl/chainjnttojacsolver.hpp>
+#include <kdl/chainfksolverpos_recursive.hpp>
+#include <kdl/chainiksolverpos_nr.hpp>
+#include <kdl/chainiksolvervel_pinv.hpp>
+
+// romocc utilities
+#include "romoccExport.hpp"
+
+#endif //ROMOCC_PRECOMPILEDHEADERS_H
