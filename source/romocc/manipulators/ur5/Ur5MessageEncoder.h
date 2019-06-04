458ea2f19145a6d756964c86aa53fce38ca1f584
Andreas Østvik <a.ostvik@gmail.com>
2019-06-04 15:24:39 +0200

    Fixed naming and added smart pointer functionality.

--- source/romocc/manipulators/ur5/Ur5MessageEncoder.h
+++ source/romocc/manipulators/ur5/Ur5MessageEncoder.h
@@ -16,28 +16,29 @@ namespace romocc
 
 class ROMOCC_EXPORT Ur5MessageEncoder : public MessageEncoder
 {
+    ROMOCC_OBJECT(Ur5MessageEncoder)
 
-public:
-    virtual std::string moveCommand(MotionType type, Eigen::RowVectorXd targetConfiguration, double acc, double vel, double t, double rad);
-    virtual std::string stopCommand(MotionType typeOfStop, double acc);
-    virtual std::string shutdownCommand();
+    public:
+        virtual std::string moveCommand(MotionType type, Eigen::RowVectorXd targetConfiguration, double acc, double vel, double t, double rad);
+        virtual std::string stopCommand(MotionType typeOfStop, double acc);
+        virtual std::string shutdownCommand();
 
-    std::string moveCommand(MotionType type, Eigen::Affine3d targetPose, double acc, double vel, double t, double rad);
+        std::string moveCommand(MotionType type, Eigen::Affine3d targetPose, double acc, double vel, double t, double rad);
 
-private:
-    std::string movej(Eigen::RowVectorXd jointConfig, double a, double v, double t, double r);
-    std::string movep(Eigen::RowVectorXd operationalConfig, double a, double v, double t, double r);
+    private:
+        std::string movej(Eigen::RowVectorXd jointConfig, double a, double v, double t, double r);
+        std::string movep(Eigen::RowVectorXd operationalConfig, double a, double v, double t, double r);
 
-    std::string speedj(Eigen::RowVectorXd jointVelocity, double a, double t);
-    std::string speedl(Eigen::RowVectorXd operationalVelocity, double a, double t);
+        std::string speedj(Eigen::RowVectorXd jointVelocity, double a, double t);
+        std::string speedl(Eigen::RowVectorXd operationalVelocity, double a, double t);
 
-    std::string stopj(double a);
-    std::string stopl(double a);
+        std::string stopj(double a);
+        std::string stopl(double a);
 
-    std::string textmsg(std::string msg);
-    std::string sleep(double time);
+        std::string textmsg(std::string msg);
+        std::string sleep(double time);
 
-    std::string powerdown();
+        std::string powerdown();
 
 };
 
