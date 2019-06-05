ba2464e77da20a993c56f2c519918bddde608096
Andreas Østvik <a.ostvik@gmail.com>
2019-06-06 00:21:54 +0200

    Changed from struct to namespace.

--- source/romocc/utilities/MathUtils.h
+++ source/romocc/utilities/MathUtils.h
@@ -3,21 +3,16 @@
 namespace romocc
 {
 
-struct TransformUtils{
-    struct Affine{
-        static Eigen::Vector3d toAxisAngle(Eigen::Affine3d affine);
-        static Eigen::Matrix<double,6,1> toVector6D(Eigen::Affine3d affine);
-        static Eigen::Affine3d scaleTranslation(Eigen::Affine3d affine, double scale);
+namespace TransformUtils{
+    namespace Affine{
+        ROMOCC_EXPORT Eigen::Vector3d toAxisAngle(Eigen::Affine3d affine);
+        ROMOCC_EXPORT Eigen::Matrix<double,6,1> toVector6D(Eigen::Affine3d affine);
+        ROMOCC_EXPORT Eigen::Affine3d scaleTranslation(Eigen::Affine3d affine, double scale);
     };
 
-    struct kdl{
-        static Eigen::Affine3d toAffine(KDL::Frame frame);
+    namespace kdl{
+        ROMOCC_EXPORT Eigen::Affine3d toAffine(KDL::Frame frame);
     };
 };
 
-//Eigen::Affine3d kdlFrameToEigenAffine(KDL::Frame frame);
-//Eigen::Affine3d scaleTranslationAffine(Eigen::Affine3d affine, double scale);
-//Eigen::Matrix<double,6,1> affineToVector6d(Eigen::Affine3d affine);
-//Eigen::Vector3d affineToAxisAngle(Eigen::Affine3d affine);
-
-}
+} // namespace romocc
