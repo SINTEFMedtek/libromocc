e9954649d08623bf0902ec0c9f20fb8e30b7a448
Andreas Østvik <a.ostvik@gmail.com>
2019-06-05 18:42:34 +0200

    Started work on tests for main object.

---
+++ source/romocc/testing/RobotTests.cpp
@@ -0,0 +1,17 @@
+//
+// Created by androst on 05.06.19.
+//
+
+#include "catch.hpp"
+#include "romocc/Robot.h"
+
+namespace romocc {
+
+TEST_CASE("Initialize robot with wrong address and check if connected", "[romocc][Robot]"){
+    Robot::pointer robot = Robot::New();
+    robot->configure(UR5, "dummy", 9999);
+    robot->start();
+    CHECK(robot->isConnected() == false);
+}
+
+}
