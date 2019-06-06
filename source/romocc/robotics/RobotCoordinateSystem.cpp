a575730da1435a8e98104a81dceb1a17fb803d77
Andreas Østvik <a.ostvik@gmail.com>
2019-06-06 10:52:16 +0200

    Added robot coordinate system convenience class.

--- source/romocc/robotics/RobotCoordinateSystem.cpp
+++ source/romocc/robotics/RobotCoordinateSystem.cpp
@@ -3,3 +3,28 @@
 //
 
 #include "RobotCoordinateSystem.h"
+
+namespace romocc
+{
+
+void RobotCoordinateSystem::set_eeMt(Eigen::Affine3d eeMt)
+{
+    this->eeMt = eeMt;
+}
+
+void RobotCoordinateSystem::set_rMb(Eigen::Affine3d rMb)
+{
+    this->rMb = rMb;
+}
+
+Transform3d RobotCoordinateSystem::get_rMb() const
+{
+    return rMb;
+}
+
+Transform3d RobotCoordinateSystem::get_eeMt() const
+{
+    return eeMt;
+}
+
+} // namespace romocc
--- source/romocc/robotics/RobotCoordinateSystem.h
+++ source/romocc/robotics/RobotCoordinateSystem.h
@@ -5,12 +5,35 @@
 #ifndef ROMOCC_ROBOTCOORDINATESYSTEM_H
 #define ROMOCC_ROBOTCOORDINATESYSTEM_H
 
+#include "romocc/core/Object.h"
+#include "Eigen/Dense"
 
+namespace romocc
+{
 
-class RobotCoordinateSystem {
+class RobotCoordinateSystem
+{
+    ROMOCC_OBJECT(RobotCoordinateSystem)
 
+    public:
+        RobotCoordinateSystem() {
+            eeMt = Eigen::Affine3d::Identity();
+            rMb = Eigen::Affine3d::Identity();
+        }
+
+        ~RobotCoordinateSystem(){};
+
+        Transform3d get_rMb() const;
+        Transform3d get_eeMt() const;
+
+        void set_eeMt(Eigen::Affine3d eeMt);
+        void set_rMb(Eigen::Affine3d rMb);
+
+    private:
+        Transform3d eeMt, rMb;
+        std::weak_ptr<RobotCoordinateSystem> mPtr;
 };
 
-
+}
 
 #endif //ROMOCC_ROBOTCOORDINATESYSTEM_H
