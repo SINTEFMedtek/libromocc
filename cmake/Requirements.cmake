9306d1e1d04fb92308963c6a863d4ca7bbc97bc3
Andreas Østvik <a.ostvik@gmail.com>
2019-06-06 14:48:53 +0200

    Respecting option.

--- cmake/Requirements.cmake
+++ cmake/Requirements.cmake
@@ -10,6 +10,10 @@ include(cmake/ExternalEigen.cmake)
 include(cmake/ExternalOrocos.cmake)
 include(cmake/ExternalZeroMQ.cmake)
 
+if(ROMOCC_BUILD_URSIMULATOR)
+    include(cmake/ExternalURSimulator.cmake)
+endif()
+
 # Make sure project can find external includes and libaries
 link_directories(${ROMOCC_EXTERNAL_INSTALL_DIR}/lib/)
 list(APPEND INCLUDE_DIRS ${ROMOCC_EXTERNAL_INSTALL_DIR}/include)
