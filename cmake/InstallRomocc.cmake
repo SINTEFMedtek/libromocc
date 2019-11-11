21ed42a814761338eb091ee2b1542ee22aee7235
androst <a.ostvik@gmail.com>
2019-11-11 13:11:07 +0100

    Initial work on Python wrapper.

--- cmake/InstallRomocc.cmake
+++ cmake/InstallRomocc.cmake
@@ -90,3 +90,16 @@ install(FILES ${LICENSE_FILES}
 install(FILES ${ROMOCC_EXTERNAL_BUILD_DIR}/orocos/src/orocos/orocos_kdl/COPYING
 		DESTINATION libromocc/licenses/orocos-kdl/
 		)
+
+# Install Python wrapper
+if(ROMOCC_BUILD_PYTHON)
+	install(TARGETS _romocc
+			DESTINATION libromocc/python/romocc
+			)
+	install(FILES ${PROJECT_BINARY_DIR}/python/romocc/romocc.py
+			DESTINATION libromocc/python/romocc
+			)
+	install(FILES ${PROJECT_BINARY_DIR}/lib/romocc/__init__.py
+			DESTINATION libromocc/python/romocc
+			)
+endif()
