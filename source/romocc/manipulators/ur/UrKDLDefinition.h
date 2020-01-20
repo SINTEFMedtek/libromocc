f991699146dd4787719111736c86d830c48578e5
androst <a.ostvik@gmail.com>
2020-01-20 13:29:34 +0100

    Moved constants to correct namespace.

--- source/romocc/manipulators/ur/UrKDLDefinition.h
+++ source/romocc/manipulators/ur/UrKDLDefinition.h
@@ -15,22 +15,22 @@ namespace Ur5
     constexpr double dh_home[6] = {0, -M_PI_2, -M_PI_2 ,-M_PI_2, M_PI_2, 0};
 
     KDL::Chain KDLChain(){
-    KDL::Chain Ur5;
-    for (unsigned int i = 0; i < 6; i++) {
-        Ur5.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ), KDL::Frame::DH(dh_a[i], dh_alpha[i], dh_d[i], 0.0)));
-    }
-    return Ur5;
+        KDL::Chain Ur5;
+        for (unsigned int i = 0; i < 6; i++) {
+            Ur5.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ), KDL::Frame::DH(dh_a[i], dh_alpha[i], dh_d[i], 0.0)));
+        }
+        return Ur5;
     }
 }
 
 namespace Ur10
 {
-    KDL::Chain KDLChain(){
-        constexpr double dh_d[6] = {0.1807, 0, 0, 0.17415, 0.11985, 0.11655};
-        constexpr double dh_a[6] = {0,-0.6127,-0.57155,0,0,0};
-        constexpr double dh_alpha[6] = {M_PI_2, 0, 0, M_PI_2, -M_PI_2, 0};
-        constexpr double dh_home[6] = {0, -M_PI_2, -M_PI_2 ,-M_PI_2, M_PI_2, 0};
+    constexpr double dh_d[6] = {0.1807, 0, 0, 0.17415, 0.11985, 0.11655};
+    constexpr double dh_a[6] = {0,-0.6127,-0.57155,0,0,0};
+    constexpr double dh_alpha[6] = {M_PI_2, 0, 0, M_PI_2, -M_PI_2, 0};
+    constexpr double dh_home[6] = {0, -M_PI_2, -M_PI_2 ,-M_PI_2, M_PI_2, 0};
 
+    KDL::Chain KDLChain(){
         KDL::Chain Ur10;
         for (unsigned int i = 0; i < 6; i++) {
             Ur10.addSegment(KDL::Segment(KDL::Joint(KDL::Joint::RotZ), KDL::Frame::DH(dh_a[i], dh_alpha[i], dh_d[i], 0.0)));
