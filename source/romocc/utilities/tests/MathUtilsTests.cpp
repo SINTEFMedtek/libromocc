bbd2043ab0b99b571d1dcb0d2dc7ffc6ad6dd623
androst <a.ostvik@gmail.com>
2020-01-27 09:38:26 +0100

    Update test with check.

--- source/romocc/utilities/tests/MathUtilsTests.cpp
+++ source/romocc/utilities/tests/MathUtilsTests.cpp
@@ -9,14 +9,18 @@ namespace romocc
 
 TEST_CASE("Convert from Vector6D to Affine to Vector6D", "[romocc][MathUtils]") {
     romocc::Vector6d vec;
-    vec << 0, 0, 100, M_PI, -M_PI_2;
+    vec << 0, 0, 100, M_PI, -M_PI_2, 0;
     std::cout << vec << std::endl;
 
     auto affine = TransformUtils::Affine::toAffine3DFromVector6D(vec);
     std::cout << affine.matrix() << std::endl;
+    std::cout << (affine*affine.inverse()).matrix() << std::endl;
 
     auto new_vec = TransformUtils::Affine::toVector6D(affine);
+    auto identity = Eigen::Affine3d::Identity();
     std::cout << new_vec << std::endl;
+
+    CHECK((affine*affine.inverse()).matrix().isIdentity(1e-6));
 }
 
 }
