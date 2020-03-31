b7188332e77dc598b8aaaf55797d123fee23e916
Andreas Østvik <a.ostvik@gmail.com>
2020-03-31 12:42:21 +0200

    Add pybind and python libs if python build option is enabled.

--- cmake/Requirements.cmake
+++ cmake/Requirements.cmake
@@ -12,6 +12,14 @@ if(ROMOCC_BUILD_URSIMULATOR)
     include(cmake/ExternalURSimulator.cmake)
 endif()
 
+if(ROMOCC_BUILD_PYTHON)
+    find_package(Python 3 REQUIRED)
+    list(APPEND ROMOCC_INCLUDE_DIRS ${PYTHON_INCLUDE_DIRS})
+    list(APPEND LIBRARIES ${PYTHON_LIBRARIES})
+
+    include(cmake/ExternalPybind.cmake)
+endif()
+
 # Make sure project can find external includes and libaries
 link_directories(${ROMOCC_EXTERNAL_INSTALL_DIR}/lib/)
 list(APPEND INCLUDE_DIRS ${ROMOCC_EXTERNAL_INSTALL_DIR}/include)
