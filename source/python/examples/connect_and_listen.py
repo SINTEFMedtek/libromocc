69c73cb011d311bbf25314d4e331bafc75876455
Andreas Østvik <a.ostvik@gmail.com>
2020-04-06 08:06:45 +0200

    Added a simple example for connecting and listening to robot.

---
+++ source/python/examples/connect_and_listen.py
@@ -0,0 +1,14 @@
+from pyromocc import Robot, Manipulator
+import time
+
+manipulator = Manipulator(Manipulator.ManipulatorType.UR10, "5.3")
+
+robot = Robot(manipulator, "192.168.153.131", 30003)
+robot.connect()
+
+print(robot.joint_config)
+time.sleep(0.5)
+print(robot.joint_config)
+time.sleep(0.5)
+
+# robot.movej([0.0, -np.pi/2, -np.pi/2, -np.pi/2, 0, 0], 50, 100)
