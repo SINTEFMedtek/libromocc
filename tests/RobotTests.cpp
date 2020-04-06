adfcc66fe129093b5c36f11e2a70c53e1831247f
Andreas Østvik <a.ostvik@gmail.com>
2020-04-06 11:50:35 +0200

    Moved tests out of source folder.

--- tests/RobotTests.cpp
+++ tests/RobotTests.cpp
@@ -2,48 +2,28 @@
 // Created by androst on 05.06.19.
 //
 
-#include "catch.hpp"
+#include "catch/catch.hpp"
 #include "romocc/Robot.h"
 #include <iostream>
 #include <thread>
 #include <mutex>
 #include <romocc/manipulators/ur/UrKDLDefinition.h>
 
+
+
 namespace romocc {
 
 TEST_CASE("Initialize robot with wrong address and check if connected", "[romocc][Robot]"){
-    Robot::pointer robot = Robot::New();
+    auto robot = Robot::New();
     robot->configure(Manipulator(), "dummy", 9999);
     robot->connect();
     CHECK(robot->isConnected() == false);
 }
 
 TEST_CASE("Initialize robot and check coordinate system", "[romocc][Robot]") {
-    Robot::pointer robot = Robot::New();
+    auto robot = Robot::New();
     CHECK(robot->getCoordinateSystem()->get_rMb().matrix() == Eigen::Affine3d::Identity().matrix());
     CHECK(robot->getCoordinateSystem()->get_eeMt().matrix() == Eigen::Affine3d::Identity().matrix());
-}
-
-TEST_CASE("Initialize robot and add update subscription", "[romocc][Robot]") {
-    auto callback = [](){};
-
-    Robot::pointer robot = Robot::New();
-    robot->configure(Manipulator(), "dummy", 9999);
-    robot->connect();
-    robot->addUpdateSubscription(callback);
-
-    void *subscriber = zmq_socket(ZMQUtils::getContext(), ZMQ_SUB);
-    zmq_connect(subscriber, "tcp://localhost:5557");
-    zmq_setsockopt(subscriber, ZMQ_SUBSCRIBE, "", 0);
-
-    std::string msg_buffer;
-    zmq_msg_t message;
-    zmq_msg_init(&message);
-    zmq_recvmsg(subscriber, &message, 0);
-    int size = zmq_msg_size(&message);
-    msg_buffer.assign((const char *) zmq_msg_data(&message), size);
-    std::cout << "Msg buffer : " << msg_buffer << std::endl;
-    // TODO: Add check
 }
 
 TEST_CASE("Initialize robot and test kinematics", "[romocc][Robot]") {
@@ -82,10 +62,8 @@ TEST_CASE("Initialize robot and listen for new states", "[romocc][Robot]") {
 TEST_CASE("Kinematics test", "[romocc][Robot]"){
     romocc::Vector6d current;
     current << -424, -564., 161., 0, 0.96*M_PI, 0.723*M_PI;
-    //std::cout << current.transpose() << "\n" << std::endl;
 
     auto affine = TransformUtils::Affine::toAffine3DFromVector6D(current);
-    //std::cout << affine.matrix() << "\n" << std::endl;
 
     auto robot = Robot::New();
     robot->configure(Manipulator(Manipulator::UR10), "192.168.153.131", 30003);
@@ -104,10 +82,6 @@ TEST_CASE("Kinematics test", "[romocc][Robot]"){
     double home[6] = {-2.37, -1.67, -1.64 ,-1.29, 0.80, 0.35};
     KDL::JntArray q_home = KDL::JntArray(6);
     for (unsigned int i = 0; i < 6; i++) {q_home(i)=home[i];}
-
-    //std::cout << q_home(0) << " " << q_home(1) << " " << q_home(2) << " " << q_home(3) << " " << q_home(4) << " " << q_home(5) << std::endl;
-    //std::cout << target_pose.M(0,0) << " " << target_pose.M(0,1) << " " << target_pose.M(0,2) << "\n" << std::endl;
-    //std::cout << target_pose.p[0] << " " << target_pose.p[1] << " " << target_pose.p[2] << "\n"<< std::endl;
 
     KDL::JntArray q_target = KDL::JntArray(6);
     int status = ik_solver->CartToJnt(q_home, target_pose, q_target);
