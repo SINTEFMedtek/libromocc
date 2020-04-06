829c93f98f2e4a7546c6cac08a331ef883b17bca
Andreas Østvik <a.ostvik@gmail.com>
2020-04-06 09:49:01 +0200

    Updated examples to respect connection states.

--- examples/RobotListen.cpp
+++ examples/RobotListen.cpp
@@ -14,32 +14,40 @@ int main(int argc, char *argv[])
     };
 
     auto ur5 = Robot::New();
-    ur5->configure(Manipulator(), "10.218.180.32", 30003);
-    ur5->connect();
-    ur5->addUpdateSubscription(print_message);
+    ur5->configure(Manipulator(), "192.168.153.131", 30003);
 
-    auto initialJointConfig = ur5->getCurrentState()->getJointConfig();
-    std::cout << initialJointConfig.transpose() << std::endl;
+    if(ur5->connect())
+    {
+        ur5->addUpdateSubscription(print_message);
 
+        auto initialJointConfig = ur5->getCurrentState()->getJointConfig();
+        std::cout << initialJointConfig.transpose() << std::endl;
 
-    double previousTime = 0;
-    bool stop = false;
-    std::thread processing_thread([&](){
-        while(!stop){
-            Vector6d jointConfig = ur5->getCurrentState()->getJointConfig();
-            double currentTime = ur5->getCurrentState()->getTimestamp();
-            Transform3d m_bM1 = ur5->getCurrentState()->getTransformToJoint(1);
-            Transform3d m_bM2 = ur5->getCurrentState()->getTransformToJoint(2);
-            Transform3d m_bM3 = ur5->getCurrentState()->getTransformToJoint(3);
-            Transform3d m_bM4 = ur5->getCurrentState()->getTransformToJoint(4);
-            Transform3d m_bM5 = ur5->getCurrentState()->getTransformToJoint(5);
-            Transform3d m_bM6 = ur5->getCurrentState()->getTransformToJoint(6);
+        double previousTime = 0;
+        bool stop = false;
+        std::thread processing_thread([&](){
+            while(!stop){
+                Vector6d jointConfig = ur5->getCurrentState()->getJointConfig();
+                double currentTime = ur5->getCurrentState()->getTimestamp();
+                Transform3d m_bM1 = ur5->getCurrentState()->getTransformToJoint(1);
+                Transform3d m_bM2 = ur5->getCurrentState()->getTransformToJoint(2);
+                Transform3d m_bM3 = ur5->getCurrentState()->getTransformToJoint(3);
+                Transform3d m_bM4 = ur5->getCurrentState()->getTransformToJoint(4);
+                Transform3d m_bM5 = ur5->getCurrentState()->getTransformToJoint(5);
+                Transform3d m_bM6 = ur5->getCurrentState()->getTransformToJoint(6);
 
-            if(currentTime > previousTime)
-            {
-                std::cout << jointConfig.transpose() << std::endl;
-                previousTime = ur5->getCurrentState()->getTimestamp();
+                if(currentTime > previousTime)
+                {
+                    std::cout << jointConfig.transpose() << std::endl;
+                    previousTime = ur5->getCurrentState()->getTimestamp();
+                }
             }
-        }
-    });
+        });
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        stop = true;
+        processing_thread.join();
+    }
+
+
 }
