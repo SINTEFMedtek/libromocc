b2e94f45a973c6b760361208670c40fed9886a99
Andreas Østvik <a.ostvik@gmail.com>
2020-05-11 08:10:39 +0200

    Updated example to print pose.

--- source/pyromocc/examples/connect_and_listen.py
+++ source/pyromocc/examples/connect_and_listen.py
@@ -1,12 +1,10 @@
 from pyromocc import Robot
 import numpy as np
 from time import sleep
+np.set_printoptions(precision=3)
 
 robot = Robot(ip="192.168.153.131", port=30003, manipulator="UR10")
 robot.connect()
-
-sleep(1.0)
-print(robot.joint_config)
 
 sleep(1.0)
 print(robot.joint_config)
@@ -15,3 +13,4 @@ robot.movej([0.0, -np.pi/2, -np.pi/2, -np.pi/2, 0, 0], 50, 100)
 
 sleep(1.0)
 print(robot.joint_config)
+print(robot.pose)
