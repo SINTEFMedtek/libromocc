3bcc2eacc927e68bfbc9c5f5ae0b7e42a6a5aff2
Andreas Østvik <a.ostvik@gmail.com>
2020-05-11 11:30:51 +0200

    Bumped patch version.

--- source/pyromocc/setup.py
+++ source/pyromocc/setup.py
@@ -10,7 +10,7 @@ else:
     package_data[package_name] = ['*.pyd', '*.dll']
 
 setup(name=package_name,
-      version='0.0.2',
+      version='0.0.3',
       author="Andreas Østvik",
       packages=find_packages(exclude=['third_party', 'examples']),
       install_requires=['numpy'],
