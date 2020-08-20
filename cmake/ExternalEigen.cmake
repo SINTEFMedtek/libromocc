f19405b88a851d7ed68e0daba3b5980677fe6869
Andreas Østvik <a.ostvik@gmail.com>
2020-08-20 15:16:37 +0200

    Updated eigen path.

--- cmake/ExternalEigen.cmake
+++ cmake/ExternalEigen.cmake
@@ -5,7 +5,7 @@ include(cmake/Externals.cmake)
 ExternalProject_Add(eigen
         PREFIX ${ROMOCC_EXTERNAL_BUILD_DIR}/eigen
         BINARY_DIR ${ROMOCC_EXTERNAL_BUILD_DIR}/eigen/build
-        URL "https://bitbucket.org/eigen/eigen/get/f3a22f35b044.tar.gz"
+        URL "https://gitlab.com/libeigen/eigen/-/archive/3.3.7/eigen-3.3.7.tar.gz"
         INSTALL_DIR ${ROMOCC_EXTERNAL_INSTALL_DIR}
         CMAKE_CACHE_ARGS
         -DCMAKE_BUILD_TYPE:STRING=Release
