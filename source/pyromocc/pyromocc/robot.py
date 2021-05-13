91b42f58f51b0e4b14b22bc28ee200057e93a3ce
Andreas Østvik <a.ostvik@gmail.com>
2021-05-13 22:45:46 +0200

    Implemented move_to_pose and unit specification on initialization. Added more documentation.

--- source/pyromocc/pyromocc/robot.py
+++ source/pyromocc/pyromocc/robot.py
@@ -15,13 +15,21 @@ class Robot(RobotBase):
         Manipulator type. Currently supports UR5 and UR10 from universal robotics. Defaults to 'UR5'.
     sw_version: str
         Controller software version. Format 'major.minor'. Defaults to "5.3".
+
+    Attributes
+    ----------
+    pose : ndarray
+        Returns the instant 4x4 homogeneous matrix from base to end-effector
+    pose_aa : list
+        A list of 6 values representing the axis-angle representation and translation of the end-effector wrt base
     """
 
-    def __init__(self, ip: str, port: int = 30003, manipulator: str = None, **kwargs):
+    def __init__(self, ip: str, port: int = 30003, manipulator: str = None, units="mm", **kwargs):
         RobotBase.__init__(self)
         self.ip = ip
         self.port = port
         self.sw_version = kwargs.get("sw_version", "5.3")
+        self.units = units  # default unit is mm (millimetre)
 
         if manipulator is None or manipulator == 'UR5':
             self.manipulator = Manipulator(ManipulatorType.UR5, self.sw_version)
@@ -31,6 +39,29 @@ class Robot(RobotBase):
             raise ValueError("Manipulator of type {} not supported.".format(manipulator))
 
         self.configure(self.manipulator, self.ip, self.port)
+
+    def move_to_pose(self, pose, acceleration, velocity):
+        """
+        Parameters
+        ----------
+        pose: ndarray
+            Move to the specified pose. Pose is a 4x4 homogeneous transform
+        acceleration: float
+            Acceleration to velocity value
+        velocity: float
+            Velocity of motion
+        """
+
+        if self.units == "mm":
+            self.movep(pose, acceleration, velocity)
+        elif self.units == "m":
+            # scale to mm before calling movep
+            pose[:3, 3] *= 1000
+            acceleration *= 1000
+            velocity *= 1000
+            self.movep(pose, acceleration, velocity)
+        else:
+            raise NotImplemented("Unit {} not supported.".format(self.units))
 
     @property
     def joint_config(self):
@@ -42,7 +73,14 @@ class Robot(RobotBase):
 
     @property
     def pose(self):
-        return self.get_state().get_pose()
+        pose = self.get_state().get_pose()
+        if self.units == "mm":
+            return pose
+        elif self.units == "m":
+            pose[:3, 3] /= 1000
+            return pose
+        else:
+            raise NotImplemented("Unit {} not supported.".format(self.units))
 
     @property
     def pose_aa(self):
