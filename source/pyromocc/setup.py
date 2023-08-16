b310974c9a499c2d9c0ba9fdbc43826d3b976772
androst <a.ostvik@gmail.com>
2023-08-16 16:06:40 +0200

    Updates to setup.py to handle linking of libraries. Currently not optimal, since build folder is hard coded.

--- source/pyromocc/setup.py
+++ source/pyromocc/setup.py
@@ -1,13 +1,19 @@
-from setuptools import setup, find_packages
 import os
+import glob
+
+from setuptools import setup, find_packages
 
 package_name = "pyromocc"
 package_data = {}
 
+library_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../build/lib'))
+
 if os.name == 'posix':
-    package_data[package_name] = ['*.so']
+    libraries = glob.glob(os.path.join(library_path, "*.so"))
+    package_data = {"pyromocc": ['*.so', *libraries]}
 else:
-    package_data[package_name] = ['*.pyd', '*.dll']
+    libraries = glob.glob(os.path.join(library_path, "*.lib"))
+    package_data[package_name] = ['*.pyd', '*.dll', '*.lib']
 
 setup(name=package_name,
       version='0.0.6',
@@ -20,9 +26,9 @@ setup(name=package_name,
       classifiers=[
           'Operating System :: POSIX :: Linux',
           'Programming Language :: C++',
-          'Programming Language :: Python :: 3.6',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
       ],
-      python_requires='>=3.6',
+      python_requires='>=3.7',
       package_data=package_data)
