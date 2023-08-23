796b9befa9aedf34a5f1151fb36af5152503c1c1
Andreas Østvik <a.ostvik@gmail.com>
2023-08-23 20:39:11 +0200

    Updated CMAKE option to be correct to avoid building tests for zmq.

--- cmake/ExternalZeroMQ.cmake
+++ cmake/ExternalZeroMQ.cmake
@@ -14,7 +14,7 @@ ExternalProject_Add(zeromq
             -DBUILD_SHARED_LIBS=OFF
             -DBUILD_STATIC_LIBS=ON
             -DBUILD_PACKAGING=OFF
-            -DBUILD_TESTING=OFF
+            -DBUILD_TESTS=OFF
             -DBUILD_NC_TESTS=OFF
             -DBUILD_CONFIG_TESTS=OFF
             -DINSTALL_HEADERS=ON
