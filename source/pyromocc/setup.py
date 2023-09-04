0682fb8c41eb22f07185bc62b86070a0b02ed7f4
Andreas Østvik <a.ostvik@gmail.com>
2023-09-04 07:38:03 +0200

    Updated package data on windows.

--- source/pyromocc/setup.py
+++ source/pyromocc/setup.py
@@ -4,9 +4,9 @@ import glob
 from setuptools import setup, find_packages, Extension
 
 package_name = "pyromocc"
-package_data = {}
 
-build_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../build'))  # Should be dynamic
+build_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))  # Should be dynamic
+bin_path = os.path.join(build_folder, 'bin')
 library_path = os.path.join(build_folder, 'lib')
 
 if os.name == 'posix':
@@ -14,23 +14,9 @@ if os.name == 'posix':
     package_data = {"pyromocc": ['*.so', *libraries]}
 else:
     libraries = glob.glob(os.path.join(library_path, "*.lib"))
-    package_data[package_name] = ['*.pyd', '*.dll', '*.lib']
-
-extension = Extension(
-    'pyromocc',
-    sources=['source/pyromocc.cpp'],
-    libraries=libraries,
-    library_dirs=[library_path],
-    include_dirs=[os.path.join(build_folder, 'third-party/pybind11/include'),
-                  os.path.join(build_folder, 'include'),
-                  os.path.join(build_folder, 'include/eigen3'),
-                  os.path.join(build_folder, 'include/kdl'),
-                  os.path.join(build_folder, '../source'),
-                  os.path.join(build_folder, '.')
-                  ],
-    extra_compile_args=[],
-    extra_link_args=['-Wl,-rpath,$ORIGIN'],
-)
+    dlls = glob.glob(os.path.join(bin_path, "*.dll"))
+    pyds = glob.glob(os.path.join(library_path, "*.pyd"))
+    package_data = {"pyromocc": ['*.pyd', '*.dll', '*.lib', *pyds, *libraries, *dlls]}
 
 setup(name=package_name,
       version='0.0.6',
@@ -42,11 +28,11 @@ setup(name=package_name,
       include_package_data=True,
       classifiers=[
           'Operating System :: POSIX :: Linux',
+          'Operating System :: Microsoft :: Windows',
           'Programming Language :: C++',
           'Programming Language :: Python :: 3.7',
           'Programming Language :: Python :: 3.8',
           'Programming Language :: Python :: 3.9',
       ],
       python_requires='>=3.7',
-      ext_modules=[extension],
       package_data=package_data)
