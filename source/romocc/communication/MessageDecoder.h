ce11f48b4a3e8d2efc5daf82f8b520f441bac046
Andreas Østvik <a.ostvik@gmail.com>
2023-09-09 19:51:47 +0200

    Renamed JointState to ConfigState and added operational config and velocity.

--- source/romocc/communication/MessageDecoder.h
+++ source/romocc/communication/MessageDecoder.h
@@ -1,23 +1,25 @@
 #ifndef ROMOCC_MESSAGEDECODER_H
 #define ROMOCC_MESSAGEDECODER_H
 
+#include "romocc/core/Object.h"
 #include "romocc/core/ForwardDeclarations.h"
-#include "romocc/robotics/RobotState.h"
 
 namespace romocc
 {
 
-struct ROMOCC_EXPORT JointState
+struct ROMOCC_EXPORT ConfigState
 {
     Eigen::RowVectorXd jointConfiguration;
     Eigen::RowVectorXd jointVelocity;
+    Eigen::RowVectorXd operationalConfiguration;
+    Eigen::RowVectorXd operationalVelocity;
     double timestamp = 0;
 };
 
 class ROMOCC_EXPORT MessageDecoder : public Object
 {
     public:
-        virtual JointState analyzeTCPSegment(unsigned char* packet) = 0;
+        virtual ConfigState analyzeTCPSegment(unsigned char* packet) = 0;
 
 };
 
