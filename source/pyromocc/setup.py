bf0273eae47bc6a5bf4e0d5771ff414f5041470d
Andreas Østvik <a.ostvik@gmail.com>
2024-03-21 13:18:23 +0100

    Updated version to 0.0.8.

--- source/pyromocc/setup.py
+++ source/pyromocc/setup.py
@@ -9,7 +9,7 @@ from setuptools.dist import Distribution
 from setuptools import setup, find_packages
 
 package_name = "pyromocc"
-version = "0.0.7"
+version = "0.0.8"
 cmdclass = {}
 
 build_folder = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
